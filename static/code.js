1454c3046ce4970a9b198ac8099ce61c12153966
Ilgam Gabdullin <ilgamgabdullin@yandex.ru>
2019-09-01 00:51:59 +0300

    commit name

--- static/code.js
+++ static/code.js
@@ -1,11 +1,6 @@
 const ws = new WebSocket('wss://acceleromate.herokuapp.com');
 const json = document.querySelector('.json');
 
-const position = {
-  x: 0,
-  y: 0
-}
-
 const main = () => {
   const isMobile = window.innerWidth < 960;
 
@@ -17,38 +12,17 @@ const main = () => {
   
   if (!isMobile) {
     const dotEl = document.querySelector('.dot');
-    let lastPosition = null;
 
     ws.onmessage = (message) => {
       let { x, y } = JSON.parse(message.data);
 
-      if (lastPosition) {
-        let { lastX, lastY } = lastPosition;
-
-        if (lastX  < x) {
-          position.x--
-        } else {
-          position.x++;
-        }
-
-        if (lastY < y) {
-          position.y++;
-        } else {
-          position.y--;
-        }
-
-        json.innerHTML = message.data;
-        lastPosition = {lastX: x, lastY: y};
-        drawDot(dotEl, {x: position.x, y: position.y});
-      } else {
-        lastPosition = {lastX: x, lastY: y};
-      }
+      drawDot(dotEl, {x, y});
     }
   }
 }
 
 const drawDot = (el, {x,y}) => {
-  el.style.transform = `translate(${-x}px, ${-y}px)`
+  el.style.transform = `translate(${x}px, ${y}px)`
 }
 
 const handleOrinationChange = (event) => {
