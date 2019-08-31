4bfc70b757d14bdc21a31d14173b8fc5f0fdeb1c
Ilgam Gabdullin <ilgamgabdullin@yandex.ru>
2019-08-31 15:31:37 +0300

    first changes

---
+++ api/index.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const express = require('express');
+const http = require('http');
+const app = express();
+const WebSocket = require('ws');
+
+const port = 3000;
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({server});
+
+app.use(express.static(path.join(__dirname + '/../static')))
+
+const clients = [];
+
+// const client = {
+//   isController: true,
+//   data: null
+// }
+
+wss.on('connection', (ws) => {
+
+  clients.push(ws);
+
+  setInterval(() => {
+    console.log(`Number of clients: ${clients.length}`);
+  }, 2000);
+  // ws.on('message', (message) => {
+    
+  // })
+
+  ws.send('new user joined us');
+});
+
+wss.on('close', () => {
+  console.log('disconected');
+});
+
+
+app.get('/index', (req, res) => {
+  return res.sendFile( path.join(__dirname + '/../index.html'));
+});
+
+server.listen(port, () => console.log('started'));
---
+++ static/code.js
@@ -0,0 +1,44 @@
+const main = () => {
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', handleOrinationChange, true);
+  } else {
+    renderData({info: 'Your device is not supported or another error happened'}, document.body);
+  }
+}
+
+const handleOrinationChange = (event) => {
+  const json = document.querySelector('.json');
+  const data = {
+    alpha: Number.parseFloat(event.alpha).toFixed(2),
+    beta: Number.parseFloat(event.beta).toFixed(2),
+    gamma: Number.parseFloat(event.gamma).toFixed(2),
+  }
+
+  renderData(data, json)
+}
+
+const renderData = (data, node) => {
+  node.innerHTML = JSON.stringify(data);
+}
+
+document.addEventListener('DOMContentLoaded', main);
+
+
+const ws = new WebSocket('ws://localhost:3000');
+
+ws.onopen = (res) => {
+  // console.log('onopen', res);
+
+  ws.send('Yo motherfucker');
+}
+
+ws.onmessage = (message) => {
+  // console.log('message', message);
+}
+
+// document.addEventListener('mousemove', (event) => {
+//   const { x, y } = event;
+
+//   ws.send(JSON.stringify({x, y}));
+  
+// });
